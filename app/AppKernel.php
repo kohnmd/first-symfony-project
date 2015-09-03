c4ab166113693d3e7684d4e95db88272d6316aad
Mike Kohn <kohnmd@gmail.com>
2015-09-02 22:59:22 -0600

    Better cache and log performance on vagrant

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -33,4 +33,34 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    /**
+     * Custom cache dir if dev/test environment to improve vagrant performance.
+     *
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        if (in_array($this->environment, array('dev', 'test'))) {
+            return '/dev/shm/appname/cache/' .  $this->environment;
+        }
+
+        return parent::getCacheDir();
+    }
+
+    /**
+     * Custom cache dir if dev/test environment to improve vagrant performance.
+     *
+     * @return string
+     */
+    public function getLogDir()
+    {
+        if (in_array($this->environment, array('dev', 'test'))) {
+            return '/dev/shm/appname/logs';
+        }
+
+        return parent::getLogDir();
+    }
+
+
 }
